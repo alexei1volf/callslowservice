198443c8239a5f8faf6a209e3e2c62e7ff22a579
alex volf <alexei1volf@gmail.com>
2020-12-22 12:02:28 +0700

    hystrix rate limiter

--- server/src/main/java/com/example/dashboard/CoronaSummaryService.java
+++ server/src/main/java/com/example/dashboard/CoronaSummaryService.java
@@ -1,6 +1,8 @@
 package com.example.dashboard;
 
 import com.example.dashboard.dto.CoronaSummaryDto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -8,6 +10,15 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class CoronaSummaryService {
 
+    @HystrixCommand(groupKey = "CoronaSummary", threadPoolKey = "CoronaSummaryPool",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"),
+                    @HystrixProperty(name = "maxQueueSize", value = "2")
+
+            })
     public CoronaSummaryDto getCoronaSummary() {
         String uriString = UriComponentsBuilder.newInstance()
                 .scheme("http")
